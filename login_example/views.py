301c9e4b1854045bf7a9289e501343ffa0f12639
harmy <hack4cn@gmail.com>
2013-06-22 19:04:29 +0800

    完善代码

--- login_example/views.py
+++ login_example/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, redirect, url_for, flash, session, abort
+#coding=utf-8
+from flask import render_template, request, redirect, url_for, flash, make_response
 from flask.ext.login import LoginManager, login_user, logout_user, current_user, login_required
 from .models import User, db_session
 from .forms import LoginForm
@@ -9,6 +10,8 @@ login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
+def chunks(l, n):
+    return [l[i:i+n] for i in range(0, len(l), n)]
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -23,11 +26,15 @@ def remove_db_session(exception=None):
 @app.route('/')
 def index():
     zonelist = None
+    zonechunks = None
+    latest_zoneid = 1
     if current_user.is_authenticated():
         r = requests.get('http://192.168.0.100/api/7c1e2648-cf39-11e2-a5db-080027880ca6/zones')
         if r.status_code == requests.codes.ok:
             zonelist = r.json()
-    return render_template('index.html', form=LoginForm(), zonelist=zonelist)
+            latest_zoneid = zonelist[0]['zoneid']
+            zonechunks = chunks(zonelist, 16)
+    return render_template('index.html', form=LoginForm(), zonechunks=zonechunks, latest_zoneid=latest_zoneid)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -39,13 +46,12 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is not None and user.valid_password(form.password.data):
-            if login_user(user, remember=form.remember.data):
-                session.permanent = not form.remember.data
+            if login_user(user, remember=False):
                 return redirect(request.args.get('next') or url_for('index'))
             else:
-                flash('This username is disabled!', 'error')
+                flash(u'该账号已被封停!', 'error')
         else:
-            flash('Wrong username or password!', 'error')
+            flash(u'错误的账号或密码!', 'error')
     return render_template('index.html', form=form)
 
 
@@ -55,9 +61,10 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/play/<int:zoneid>')
+@app.route('/play')
 @login_required
-def play(zoneid):
+def play():
+    zoneid = request.args.get('zoneid', 1)
     r = requests.get('http://192.168.0.100/api/7c1e2648-cf39-11e2-a5db-080027880ca6/authorize?username={0}&zoneid={1}'.format(current_user.username, zoneid))
     gateinfo = r.json() if r.status_code == requests.codes.ok else None
     if gateinfo is None:
@@ -67,10 +74,10 @@ def play(zoneid):
 @app.route('/play/ok')
 @login_required
 def playok():
-    return '{0}'.format(request.args['token'])
+    return '{0}'.format(request.args.get('token'))
 
 @app.route('/play/error')
 @login_required
 def playerror():
-    err_message = request.args['message'] or ''
+    err_message = request.args.get('message')
     return 'error:{0}'.format(err_message)
